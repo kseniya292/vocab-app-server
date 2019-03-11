eef695ca74f81877039c8259a68e42ab2cb8c6b8
Kseniya Lifanova <kseniya@upstate.agency>
2019-03-10 21:07:03 -0700

    finished define function

--- api/controllers/WordController.js
+++ api/controllers/WordController.js
@@ -8,10 +8,10 @@ const rp = require('request-promise');
 
 module.exports = {
   define: function (req, res) {
-    const word = req.body.vocabword;
+    const vocabword = req.body.vocabword;
 
     const options = {
-      url: 'https://od-api.oxforddictionaries.com/api/v1/entries/en/' + word,
+      url: 'https://od-api.oxforddictionaries.com/api/v1/entries/en/' + vocabword,
       headers: {
         'app_id': '3e23ba37',
         'app_key': '90da04772846856a4fc02a7146753d12'
@@ -21,7 +21,19 @@ module.exports = {
 
     rp(options)
     .then(response => {
-      res.send(response);
+      let word = {};
+      word.name = vocabword;
+      word.type = response.results[0].lexicalEntries[0].lexicalCategory;
+      word.definition = [];
+      word.examples = [];
+      let senses = response.results[0].lexicalEntries[0].entries[0].senses;
+
+      Object.values(senses).forEach(value => {
+        word.definition.push(value.definitions[0]);
+        word.examples.push(value.examples[0].text);
+      });
+
+      res.send(word);
     })
     .catch(err => {
       res.send(err.statusCode);
